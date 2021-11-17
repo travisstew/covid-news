7eb5201a09f1c7d34304400029598cc7487a831d
Travis <travisstew214@gmail.com>
2021-11-16 19:58:48 -0500

    update

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const { send } = require('process');
-// const cookieParser = require('cookie-parser');
+// const { send } = require('process');
+const cookieParser = require('cookie-parser');
 
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cookieParser());
 app.use(cors());
+
 
 //body parser
 app.use(express.urlencoded({extended:false}));
